ed822cb0a112f01ff4a63fc6e799a69cd4f79528
JackFaiSeT <archakov.andrey@lab-company.ru>
2022-07-06 20:00:20 +0300

    feat: add footer

---
+++ src/components/Footer.vue
@@ -0,0 +1,24 @@
+<template>
+  <v-card
+    flat
+    tile
+    width="100%"
+    class="red lighten-1 text-center"
+  >
+    <v-divider />
+    <v-card-text class="white--text">
+      {{ currentYear }} — <strong>Vuetify</strong>
+    </v-card-text>
+  </v-card>
+</template>
+
+<script>
+export default {
+  name: 'Footer',
+  computed: {
+    currentYear() {
+      return new Date().getFullYear()
+    }
+  }
+}
+</script>
