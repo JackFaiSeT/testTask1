bf58cce4d551d98f62c77eae3e3b01a4e038c9e3
JackFaiSeT <archakov.andrey@lab-company.ru>
2022-07-06 19:59:43 +0300

    feat: add filters

---
+++ src/components/SelectFilterGroup.vue
@@ -0,0 +1,34 @@
+<template>
+<div>
+  <v-select
+    v-for="(filter) in filters"
+    v-model="filter.model"
+    :key="filter.id"
+    :items="filter.items"
+    :label="filter.label"
+    clearable
+    @input="updateData"
+  />
+</div>
+</template>
+
+<script>
+export default {
+  name: 'SelectFilterGroup',
+  props: {
+    filters: {
+      type: Array,
+      default: () => []
+    }
+  },
+  methods: {
+    updateData() {
+      const result = {}
+      this.filters.forEach(item => {
+        result[item.id] = item.model
+      })
+      this.$emit('update', result)
+    }
+  }
+}
+</script>
