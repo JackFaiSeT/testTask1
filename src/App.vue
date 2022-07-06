5b20eaf70f280f053196364c214a2c00f93dfc6d
JackFaiSeT <archakov.andrey@lab-company.ru>
2022-07-06 20:00:32 +0300

    feat: create app

---
+++ src/App.vue
@@ -0,0 +1,38 @@
+<template>
+  <v-app>
+    <v-main>
+      <section-users/>
+    </v-main>
+
+    <v-footer app v-bind="localAttrs">
+      <Footer />
+    </v-footer>
+  </v-app>
+</template>
+
+<script>
+import SectionUsers from './views/SectionUsers';
+import Footer from './components/Footer';
+
+export default {
+  name: 'App',
+  components: {
+    SectionUsers,
+    Footer
+  },
+  computed: {
+    localAttrs () {
+      const attrs = {}
+
+      attrs.absolute = true
+      attrs.fixed = false
+
+      return attrs
+    },
+  },
+
+  data: () => ({
+    //
+  }),
+};
+</script>
